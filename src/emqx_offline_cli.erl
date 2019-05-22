211e8579c7d1b06949dd6708e1ba5ab0afa1d69b
Ali Imran <ali.alfred@gmail.com>
2019-05-22 05:38:07 +0500

    EMQ X Offline 14

--- src/emqx_offline_cli.erl
+++ src/emqx_offline_cli.erl
@@ -2,13 +2,11 @@
 -module(emqx_offline_cli).
 
 -include_lib("emqx/include/emqx_client.hrl").
--include_lib("emqx/include/emqx_cli.hrl").
--include_lib("emqx/include/logger.hrl").
 
 -export([cmd/1]).
 
 cmd(["arg1", "arg2"]) ->
-    ?PRINT_MSG("ok");
+    emqx_cli:print("ok");
 
 cmd(_) ->
-    ?USAGE([{"cmd arg1 arg2", "cmd demo"}]).
+    emqx_cli:usage([{"cmd arg1 arg2", "cmd demo"}]).
