23805d26e725eaee654d26d1f4776c61c69b7659
Ali Imran <ali.alfred@gmail.com>
2019-05-22 05:30:12 +0500

    EMQ X Offline 09

--- src/emqx_offline_auth.erl
+++ src/emqx_offline_auth.erl
@@ -3,12 +3,14 @@
 -behaviour(emqx_auth_mod).
 
 -include_lib("emqx/include/emqx.hrl").
+-include_lib("emqx/include/emqx_client.hrl").
+-include_lib("emqx/include/emqx_mqtt.hrl").
 
 -export([init/1, check/3, description/0]).
 
 init(Opts) -> {ok, Opts}.
 
-check(#{client_id := ClientId, username := Username}, Password, _Opts) ->
+check(#mqtt_client{client_id = ClientId, username = Username}, Password, _Opts) ->
     io:format("Auth Demo: clientId=~p, username=~p, password=~p Opts=~p~n",
               [ClientId, Username, Password, _Opts]),
     ok.
