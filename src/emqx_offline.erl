5245c44e3132bafa1c12668d4623b1f1f2dd2a15
Ali Imran <ali.alfred@gmail.com>
2019-05-23 15:23:44 +0500

    EMQ X Offline 36

--- src/emqx_offline.erl
+++ src/emqx_offline.erl
@@ -51,7 +51,7 @@ on_message_publish(Message, _Env) ->
           Message1 = emqx_message:make(?MODULE, ?PUSH_NOTIFICATION_TOPIC, Payload),
           Res = emqx_broker:publish(Message1);
 %%          lager:info("[Offline] ~p: Redirecting the message to the topic '~s': ~p", [?MODULE, ?PUSH_NOTIFICATION_TOPIC, Res]);
-        [#trie_node{topic = Topic} ->
+        [#trie_node{topic = Topic}] ->
           ok
       end,
     {ok, Message}.
